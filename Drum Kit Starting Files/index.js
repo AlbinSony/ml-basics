a40369a7917ea56a1dc02a9ff2ebebc1142d5714
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2023-09-22 22:54:25 +0530

    sound trial added

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,7 +4,8 @@ for(var i=0;i<=numberOfDrumButtons;i++){
 
 
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    var audio=new Audio()
+    var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
 });
 
 }
